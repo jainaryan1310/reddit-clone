72e11786b262edb038adfe697ee73771c3abed7f
Aryan Jain <jainaryan1310@gmail.com>
2021-07-11 03:57:36 +0530

    initializing graphql code generator

--- lireddit-web/src/components/InputField.tsx
+++ lireddit-web/src/components/InputField.tsx
@@ -23,15 +23,11 @@ export const InputField: React.FC<InputFieldProps> = ({
   size: _,
   ...props
 }) => {
-  let InputOrTextarea = Input;
-  if (textarea) {
-    InputOrTextarea = Textarea;
-  }
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <InputOrTextarea {...field} {...props} id={field.name} />
+      <Input {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
